0d2581b98fd15ae38c4990002595c2943c12dd50
Melanie Burger <melanie.brgr@gmail.com>
2016-01-30 17:22:16 -0500

    show clickable youtube thumbnail on submit

--- js/app.js
+++ js/app.js
@@ -2,28 +2,32 @@
 $(function() {
 
 	function showResults( data ) {
+		$('.results').empty();
 		var html = "";
 		$.each(data, function(i, el) {
-			html += "<div>" + el.snippet.title + "</div>";
+			var img = el.snippet.thumbnails.medium;
+			var videoId = el.id.videoId;
+			html += "<a href='https://www.youtube.com/watch?v="+videoId+"' target='_blank' ><div style='background-image: url("+img.url+");' ><h2>" + el.snippet.title + "</h2></div></a>";
 		});
 		$('.results').html(html);
 	}
 
-	function getRequest() {
+	function getRequest( query) {
 		var url = "https://www.googleapis.com/youtube/v3/search";
 		var params = {
 			part: 'snippet',
 			key: 'AIzaSyCppL3MRqDJByS_MIWwObcyxZDwRcE7y5k',
-			q: 'Adele'
+			q: query,
+			type: 'video'
 		};
 
 		$.getJSON(url, params, function(data){
 			showResults(data.items);
+			console.log(data.items);
 		});
 	}
 	$('#search-form').submit(function(e) {
 		e.preventDefault();
 		getRequest( $(this).find('#query').val() );
 	});
-
 });
